8d6b86430db414afc02c39b084b8e91ce5927f08
ooisv@naver.com <ooisv@naver.com>
2022-11-15 11:19:15 +0900

    11.15  11:19 슬빈
    .

--- app/src/main/java/com/example/berp_and/login/LoginFragment.java
+++ app/src/main/java/com/example/berp_and/login/LoginFragment.java
@@ -1,4 +1,4 @@
-package com.example.berp_and;
+package com.example.berp_and.login;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -6,6 +6,8 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.fragment.app.Fragment;
+
+import com.example.berp_and.R;
 
 public class LoginFragment extends Fragment {
 
