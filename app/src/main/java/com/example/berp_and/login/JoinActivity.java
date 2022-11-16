67ff2e8827bcd824e1ea8bbc273167b70eb24aa5
ooisv@naver.com <ooisv@naver.com>
2022-11-16 20:17:25 +0900

    11-16 20:17
    슬빈왓다감

--- app/src/main/java/com/example/berp_and/login/JoinActivity.java
+++ app/src/main/java/com/example/berp_and/login/JoinActivity.java
@@ -3,6 +3,7 @@ package com.example.berp_and.login;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -21,16 +22,11 @@ import java.util.HashMap;
 
 public class JoinActivity extends AppCompatActivity {
 
-
-
-    String state;
-
-    Button btn_join;
+    Button btn_join, btn_id_check;
     EditText join_name, join_id, join_password, join_email, join_phone, join_addr;
     RadioGroup checkbox;
     String gender;
 
-    ArrayList<JoinDTO> joinList = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,6 +41,7 @@ public class JoinActivity extends AppCompatActivity {
         join_addr = findViewById(R.id.addr);
         checkbox = findViewById(R.id.checkbox);
         btn_join = findViewById(R.id.btn_join);
+        btn_id_check = findViewById(R.id.btn_id_check);
 
         if(checkbox.getCheckedRadioButtonId() == R.id.rdb_man){
             gender = "남";
@@ -52,19 +49,20 @@ public class JoinActivity extends AppCompatActivity {
             gender = "여";
         }
 
-        joinList.add(new JoinDTO(join_email.getText()+"", join_id.getText()+"", join_password.getText()+"",
-                join_name.getText()+"", gender, join_phone.getText()+"", join_addr.getText()+""));
-
-
         btn_join.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-
-
-
                 CommonAskTask askTask = new CommonAskTask("andJoin.mem", JoinActivity.this);
-                askTask.addParam("list", new Gson().toJson(joinList));
+                JoinDTO dto = new JoinDTO(join_email.getText()+""
+                                        , join_id.getText()+""
+                                        , join_password.getText()+""
+                                        , join_name.getText()+""
+                                        , gender+""
+                                        , join_phone.getText()+""
+                                        , join_addr.getText()+"");
+                askTask.addParam("dto" , new Gson().toJson(dto));
+
                 askTask.executeAsk(new CommonAskTask.AsynkTaskCallback() {
                     @Override
                     public void onResult(String data, boolean inResult) {
@@ -81,5 +79,7 @@ public class JoinActivity extends AppCompatActivity {
 
             }
         });
+
+
     }
 }
